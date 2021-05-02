ec371c72f699050fc3ee23c9ac5ab883a9053039
pozm <44528100+pozm@users.noreply.github.com>
2021-05-02 13:37:42 +0100

    module scripts and fix for invalid file names

--- RobloxScriptGrabber/Program.cs
+++ RobloxScriptGrabber/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using RobloxFiles;
 
 namespace RobloxScriptGrabber
@@ -9,11 +10,29 @@ namespace RobloxScriptGrabber
     internal class Program
     {
 
+        public static string RemoveInvalidFilePathCharacters(string filename, char replaceChar)
+        {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(filename, replaceChar.ToString());
+        }
+
+        public static string GetShortendScriptName(ModuleScript script)
+        {
+            return "M";
+        }
         public static string GetShortendScriptName(Script script)
         {
             return script.ClassName == "Script" ? "S" : "C";
         }
-        
+        private static void Save(ModuleScript script,string newPath)
+        {
+            File.WriteAllBytes(Path.Combine(newPath,$"{GetShortendScriptName(script)}_{ RemoveInvalidFilePathCharacters(script.Name,'_') }[{script.ScriptGuid}].lua"), script.Source);
+        }
+        private static void Save(Script script,string newPath)
+        {
+            File.WriteAllBytes(Path.Combine(newPath,$"{GetShortendScriptName(script)}_{ RemoveInvalidFilePathCharacters(script.Name,'_') }[{script.ScriptGuid}].lua"), script.Source);
+        }
         public static void Main(string[] args)
         {
             if (args.Length < 2)
@@ -37,12 +56,18 @@ namespace RobloxScriptGrabber
             
             RobloxFile file =RobloxFile.Open(args[0]);
 
-            var scripts = file.GetDescendantsOfType<Script>();
-            foreach (Script script in scripts)
+            var scScripts = file.GetDescendantsOfType<Script>();
+            var moduleScripts = file.GetDescendantsOfType<ModuleScript>();
+            
+            foreach (Script script in scScripts)
             {
-                File.WriteAllBytes(Path.Combine(newPath,$"{Program.GetShortendScriptName(script)}_{script.Name}[{script.ScriptGuid}].lua"), script.Source);
+                Save(script,newPath);
             }
-            Console.WriteLine($"Done. Saved {scripts.Length} scripts.");
+            foreach (ModuleScript script in moduleScripts)
+            {
+                Save(script,newPath);
+            }
+            Console.WriteLine($"Done. Saved {scScripts.Length + moduleScripts.Length} scripts.");
             
         }
     }
